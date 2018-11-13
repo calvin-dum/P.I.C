c41a62902f6ac3e9e31802de8d5f5f33e9e83302
calvin-dum <34903209+calvin-dum@users.noreply.github.com>
2018-11-13 15:43:51 +0100

    Add files via upload

---
+++ Flatcalv/Random_Number_Generation/rand_function.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <random>
+#include <cmath>
+int main()
+{
+    std::random_device rd{};
+    std::mt19937 gen{rd()};
+
+    // values near the mean are the most likely
+    // standard deviation affects the dispersion of generated values from the mean
+    std::normal_distribution<double> d{0,0.001};
+
+    std::map<int, int> hist{};
+    for(int n=0; n<10000; ++n) {
+        ++hist[std::round(d(gen))];
+    }
+    for(auto p : hist) {
+        std::cout << std::setw(2)
+                  << p.first << ' ' << std::string(p.second/200, '*') << '\n';
+    }
+return 0;
+}
